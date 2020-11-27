bade8ee73ca3788942f22fd3688558a7fa49b441
Joao Vitor Fernandes <up201806724@fe.up.pt>
2020-11-27 09:43:59 +0000

    db with posts missing, starting to insert values

---
+++ g21_create.sql
@@ -0,0 +1,60 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE account (
+  id INTEGER PRIMARY KEY,
+  email TEXT NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE location (
+  id INTEGER PRIMARY KEY,
+  city TEXT,
+  district TEXT
+);
+
+CREATE TABLE person (
+  person_id INTEGER PRIMARY KEY,
+  name TEXT,
+  account_id INTEGER,
+  location_id INTEGER,
+  FOREIGN KEY (account_id) REFERENCES account(id),
+  FOREIGN KEY (location_id) REFERENCES location(id)
+);
+
+CREATE TABLE shelter (
+  shelter_id INTEGER PRIMARY KEY,
+  name TEXT,
+  account_id INTEGER,
+  location_id INTEGER,
+  FOREIGN KEY (account_id) REFERENCES account(id),
+  FOREIGN KEY (location_id) REFERENCES location(id)
+);
+
+CREATE TABLE breed(
+  id INTEGER PRIMARY KEY,
+  type TEXT,
+  name TEXT
+);
+
+CREATE TABLE pet (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  gender TEXT,
+  weight REAL,
+  height REAL,
+  color TEXT,
+  breed_id INTEGER,
+  has_for_adoption INTEGER,
+  adopted INTEGER,
+  FOREIGN KEY (breed_id) REFERENCES breed(id),
+  FOREIGN KEY (has_for_adoption) REFERENCES account(id),
+  FOREIGN KEY (adopted) REFERENCES account(id)
+);
+
+create TABLE favourite(
+  pet_id INTEGER,
+  person_id INTEGER,
+  FOREIGN KEY (pet_id) REFERENCES pet(id),
+  FOREIGN KEY (person_id) REFERENCES person(id),
+  PRIMARY KEY (pet_id,person_id)
+);
---
+++ g21_insert.sql
@@ -0,0 +1,23 @@
+
+-- Accounts
+
+INSERT INTO account VALUES(NULL, 'joao@gmail.com','joao123');
+INSERT INTO account VALUES(NULL, 'bernardo@gmail.com','bernardo123');
+INSERT INTO account VALUES(NULL, 'jessica@gmail.com','jessica123');
+INSERT INTO account VALUES(NULL, 'catarina@gmail.com','catarina123');
+
+-- Location
+
+INSERT INTO location VALUES(NULL, 'Amarante','Porto');
+INSERT INTO location VALUES(NULL, 'Baião','Porto');
+INSERT INTO location VALUES(NULL, 'Felgueiras','Porto');
+INSERT INTO location VALUES(NULL, 'Gondomar','Porto');
+INSERT INTO location VALUES(NULL, 'Lousada','Porto');
+INSERT INTO location VALUES(NULL, 'Lousada','Abrantes');
+INSERT INTO location VALUES(NULL, 'Lousada','Alcanena');
+INSERT INTO location VALUES(NULL, 'Lousada','Almeirim');
+INSERT INTO location VALUES(NULL, 'Lousada','Alpiarça');
+
+
+
+
