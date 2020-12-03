b827e12849115b22a00d502e78daac8c5a17ae5d
Joao Vitor Fernandes <up201806724@fe.up.pt>
2020-12-03 11:16:21 +0000

    login.php

--- login.php
+++ login.php
@@ -1,3 +1,15 @@
 <?php
     $db = new PDO('sqlite:g21.db');
+
+    $stmt = $db->prepare('SELECT password FROM account WHERE email = ?');
+    $stmt->execute(array($_POST['email']));
+    $passwd = $stmt->fetch();
+
+    $success = $passwd['password']===$_POST['password'];
+
+    if(empty($passwd) || !$success)
+        echo '<h1>Wrong email or password</h1>';
+        
+    if($success)
+        echo '<h1>Signed in</h1>';
 ?>
