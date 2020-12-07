0583a9911323c2a478e6c2d1f8dc69c0d5cf1f4e
BernardoFerreira00 <bernardoferreira2013@gmail.com>
2020-12-07 23:00:56 +0000

    added animal to index

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    include('database/connection.php');
+    include_once('templates/common/header.php');
+
+    $webPets = getAllPetsPostsFromWebsite();
+?>
+    <?php foreach( $webPets as $webPet) { ?>
+        <img src="images/pets/original/<?=$webPet['id']?>.jpg" alt="pet<?=$webPet['id']?>" width="100" height="100">
+        <a href="petprofile.php?id=<?=$webPet['id']?>"><?=$webPet['name']?></a>
+    <?php } ?>
+    <body>
+    
+<?php
+    
+    function getAllPetsPostsFromWebsite() { 
+        global $db;
+        $stmt = $db->prepare('SELECT DISTINCT P.id, P.name 
+                            FROM shelter S, person Per, pet P 
+                            WHERE Per.account_id = P.has_for_adoption or
+                                S.account_id = P.has_for_adoption'
+                            );
+        $stmt->execute();
+        $pets = $stmt->fetchAll();
+        
+        return $pets;
+    }
+
+    include_once('templates/common/footer.php');
+?>
