acd6235874947ce35559cd09483d7199ac036f4c
Joao Vitor Fernandes <up201806724@fe.up.pt>
2020-12-08 19:33:34 +0000

    fixed directories path

--- pages/index.php
+++ pages/index.php
@@ -1,7 +1,7 @@
 <?php
-    include('database/db_user.php');
-    include_once('templates/common/header.php');
-    include_once('templates/template-posts.php');
+    include('../database/db_user.php');
+    include_once('../templates/common/header.php');
+    include_once('../templates/template-posts.php');
 
     $webPets = getAllPetsPostsFromWebsite();
     
@@ -9,12 +9,11 @@
 
 <?php foreach($webPets as $webPet) { ?>
     <a href="petprofile.php?id=<?=$webPet['id']?>">
-        <img src="images/pets/original/<?=$webPet['id']?>.jpg" alt="pet<?=$webPet['id']?>" width="100" height="100">
+        <img src="../images/pets/original/<?=$webPet['id']?>.jpg" alt="pet<?=$webPet['id']?>" width="100" height="100">
     </a>
     <h3><?=$webPet['name']?></h3>
 <?php } ?>
 
 <?php   
-    include_once('templates/common/footer.php');
+    include_once('../templates/common/footer.php');
 ?>
-<?php header('Location: pages/index.php') ?>
