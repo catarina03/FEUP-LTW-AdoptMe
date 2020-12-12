e3fc4788b563c872386fa213231def245a5cb5e1
jessymireie <up201806723@fe.up.pt>
2020-12-12 10:52:48 +0000

    userProfile

--- templates/tpl_userprofile.php
+++ templates/tpl_userprofile.php
@@ -6,9 +6,9 @@
         </header>
         <p id="bio"><?php echo $user['bio'] ?></p>
         <p id="location"><?php echo $user['city'] ?></p>
-        <p id="user_info">
+        <div id="user_info">
             <h3>XX followers</h3>
             <h3>XX following</h3>
-        </p>
+        </div>
 
 <?php } ?>
