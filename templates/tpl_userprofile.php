e40334227b778f090779c5cb4749240ecabad117
BernardoFerreira00 <bernardoferreira2013@gmail.com>
2020-12-14 15:56:58 +0000

    resolved conflicts

--- templates/tpl_userprofile.php
+++ templates/tpl_userprofile.php
@@ -1,14 +1,23 @@
 <?php function drawUserProfile($user){ ?>
+<<<<<<< HEAD
     <header>
         <img src="../images/accounts/small/<?=$user['id']?>.jpg" alt="profile picture" width="80" onerror="this.onerror=null;this.src='../images/missing_image.jpg';">
         <h2><?php echo $user['name'] ?></h2>
     </header>
     <p id="bio"><?php echo $user['bio'] ?></p>
     <p id="location"><?php echo $user['city'] ?></p>
-    <div id="user_info">
-        <h3>XX followers</h3>
-        <h3>XX following</h3>
-    </div>
+=======
+    
+        <header>
+            <img src="../images/accounts/small/<?=$user['id']?>.jpg" alt="profile picture" width="80" onerror="this.onerror=null;this.src='../images/missing_image.jpg';">
+            <h2><?php echo $user['name'] ?></h2>
+        </header>
+        <p id="bio"><?php echo $user['bio'] ?></p>
+        <p id="location"><?php echo $user['city'] ?></p>
+        <div id="user_info">
+            <h3>XX followers</h3>
+            <h3>XX following</h3>
+        </div>
 
 <?php } ?>
 
@@ -28,4 +37,5 @@
     <?php } ?>
     </ul>
 
+>>>>>>> requests
 <?php } ?>
